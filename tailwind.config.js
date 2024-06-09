4ad4179f089b6765f9fca329cf912032aba2176c
sailendraprasath <162896299+sailendraprasath@users.noreply.github.com>
2024-06-09 22:28:50 +0530

    ss
    ss

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        MyMarket1: "#95D2B3",
+        market: "#55AD9B",
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "3rem",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
